f6dd3405d93581fd5bdf6d81ce2bba856060e5d2
Feri S <48196190+feri27@users.noreply.github.com>
2025-08-26 03:57:07 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+os.environ["MKL_THREADING_LAYER"] = "GNU"
+
 import argparse
 from datetime import datetime
 import logging
